7ac82e3c486c20c3e287843edefb667bd19669c2
David Zwe Lin Htet <47786140+ZweatMonash@users.noreply.github.com>
2021-10-11 11:56:29 +1000

    fix grammar in pitch

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -10,13 +10,12 @@ const Hero = () => {
         <h1>Background</h1>
         <p>
           &emsp; &emsp;
-          <br></br> &emsp; &emsp; A Mechatronics Engineer highly experienced in
-          the field of robotics motion, sensors integration, and software
-          development lifecycle. Working in an agile start-up to deliver
-          agriculture robots to enable farmers with the latest robotics
-          technology which increases crop productivity and yield. Actively
-          invovled in client-facing roles to better understand user requirements
-          for priortising future feature development.
+          <br></br> &emsp; &emsp; A Mechatronics Engineer highly experienced in the field
+          of robotics motion, sensors integration, and software development lifecycle.
+          Working in an agile start-up to deliver agriculture robots to enable farmers
+          with the latest robotics technology which increases crop productivity and yield.
+          Actively involved in client-facing roles to better understand user requirements
+          for prioritizing future feature development.
         </p>
       </div>
     </div>
